3be7461f670d4fcac70ac62ae4d044000f0cca01
Patrick Kim <pkim050@gmail.com>
2019-07-27 17:58:23 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { directors } from '../data';
 
+const directorsArr = (directors) => {
+  return directors.map(director => {
+    return (
+      <div className="director">
+        <p>Name: {director.name}</p>
+        <ul>
+          {director.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  })
+}
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorsArr(directors)}
     </div>
   );
 }
