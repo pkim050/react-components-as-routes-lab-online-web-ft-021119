5b950f079607101fbbd5a11fbc91f7fad0fc7260
Patrick Kim <pkim050@gmail.com>
2019-07-27 18:01:04 +0000

    Automatically backed up by Learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,13 +13,13 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      <div>
+      <React.Fragment>
         <NavBar />
         <Route exact path="/" component={Home} />
         <Route exact path="/actors" component={Actors} />
         <Route exact path="/directors" component={Directors} />
         <Route exact path="/movies" component={Movies} />
-      </div>
+      </React.Fragment>
     </Router>
   );
 };
