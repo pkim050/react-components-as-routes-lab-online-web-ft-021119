9f1b46688e974c4c732e3b5f2bf539e73cf9acea
Patrick Kim <pkim050@gmail.com>
2019-07-27 17:46:56 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,10 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <NavLink to="/" exact>Home</NavLink>
+      <NavLink to="/movies" exact>Movies</NavLink>
+      <NavLink to="/directors" exact>Directors</NavLink>
+      <NavLink to="/actors" exact>Actors</NavLink>
     </div>
   );
 };
